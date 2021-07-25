6d25d709c0a7eacab879b622dd69c5c1620e2747
bencooper6851 <63478578+bencooper6851@users.noreply.github.com>
2021-07-25 18:04:26 +0300

    config fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx$/,
-})
-
-module.exports = withMDX({
-  reactStrictMode: true,
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-})
-
 module.exports = {
   webpack(config) {
     config.module.rules.push({
