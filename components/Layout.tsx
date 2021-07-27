18df2a46ef1040e36d803fdf14e9fd12c50b9152
bencooper6851 <63478578+bencooper6851@users.noreply.github.com>
2021-07-27 17:17:44 +0300

    contacts

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,28 +1,28 @@
 import Head from "next/head"
 import { FC } from "react"
 import GithubIcon from "../public/github.svg"
-import TwitterIcon from "../public/twitter.svg"
+import TwitterIcon from "../public/twitterIcon.svg"
 import { ExternalLink } from "./ExternalLink"
 
 const Layout: FC = ({ children }) => {
   return (
-    <div className='mx-auto flex flex-col space-y-10'>
+    <div className='flex flex-col mx-auto space-y-10'>
       <Head>
         <title>My Solutions Blog</title>
         <meta name='description' content='A blog' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='absolute flex flex-row right-0 m-2 space-x-2 md:h-8 h-5 w-16'>
+      <div className='absolute right-0 flex flex-row m-2 space-x-2'>
         <ExternalLink href='https://twitter.com/_bncpr'>
-          <TwitterIcon className='w-full h-full h-fuw-full fill-current text-blue-500' />
+          <TwitterIcon className='w-8 h-8 text-blue-500 fill-current' />
           <span className='sr-only'>Twitter Account</span>
         </ExternalLink>
         <ExternalLink href='https://github.com/bncpr/my-solutions-blog-next'>
-          <GithubIcon className='w-full h-full  fill-current text-gray-600 p-1' />
+          <GithubIcon className='w-8 h-8 text-purple-600 fill-current' />
           <span className='sr-only'>GitHub Repository</span>
         </ExternalLink>
       </div>
-      <h1 className='font-mono md:self-center text-6xl mt-8 px-2 md:px-0'>
+      <h1 className='px-2 mt-8 font-mono text-6xl md:self-center md:px-0'>
         My Solutions Blog.
       </h1>
 
